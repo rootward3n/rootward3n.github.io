d499e32255e987d2fac4b3c282eb6ff378641445
rootward3n <digicartelecom@gmail.com>
2026-08-13 23:50:46 +0530

    Fix Site Issue

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,11 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === "production";
-
 const nextConfig: NextConfig = {
   output: "export",
   trailingSlash: true,
-  images: { unoptimized: true },
-  basePath: isProd ? "/portfolio" : "",
-  assetPrefix: isProd ? "/portfolio/" : "",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
